a55ae8c8b858fe480d8f91242acbc37158e14d44
Ozgur Altinok <altinokozgur@gmail.com>
2015-07-03 12:07:25 -0400

    Minor Updates

---
+++ CCProtonPi0/NTupleAnalysis/Input/submitJobs_Odd.sh
@@ -0,0 +1,19 @@
+CCPROTONPI0_V="v2_13c"
+OUTPUTFOLDER="run"
+OTHEROPTIONS=""
+
+echo "Submitting Jobs ..."
+echo "... CCProtonPi0 Version: ${CCPROTONPI0_V}" 
+echo "... Output Folder: ${OUTPUTFOLDER}"
+
+runNumber_first=10201
+runNumber_last=10215
+
+for ((runNumber=${runNumber_first}; runNumber <= ${runNumber_last}; runNumber=runNumber+2 )); do
+        echo "Submitting Run = ${runNumber}"
+        $ana_script/ProcessAna.py --mc --run ${runNumber} --usecat --ana_tool CCProtonPi0 --inv eroica --use_jobsub_client --dcachein --outdir /minerva/data/users/oaltinok/CCProtonPi0/MC/${CCPROTONPI0_V}/${OUTPUTFOLDER} ${OTHEROPTIONS}
+done
+
+echo "All Runs Submitted!"
+
+
